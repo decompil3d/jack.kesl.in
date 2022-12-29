07736044ee5b75ba86390dd3a0b95fe7f6ed0d0b
Jack <jack.keslin@gmail.com>
2022-12-28 20:36:19 -0800

    End of day 2
    Co-authored-by: Jonathan Keslin <decompil3d@users.noreply.github.com>

--- games/math/patterns/index.js
+++ games/math/patterns/index.js
@@ -1,20 +1,35 @@
 const patternLength = 10;
+const numBlanks = 3;
 const main = document.getElementById('main');
-const pattern = makePattern(randomNumber());
+const pattern = makePattern(randomNumber(1, 10));
 main.innerHTML = pattern.join(", ");
 
 function makePattern(countBy) {
-  const start = randomNumber();
+  const start = randomNumber(1, 50);
   const list = [];
   for (let i = 0; i < patternLength; i++) {
     list.push(start + (i * countBy));
   }
   return list;
 }
-function randomNumber() {
-  const start = 1;
-  const end = 100;
+
+/**
+ * gets a random number between start and end
+ * @param {number} start the smallest number possible
+ * @param {number} end   the biggest number possible 
+ * @returns {number}     a random number
+ */
+function randomNumber(start, end) {
   const range = end - start;
   const rand = Math.random();
   return Math.floor(rand * range);
+}
+
+function getBlankIndices() {
+  /** @type {Set<number>} */
+  const indices = new Set();
+  while (indices.size < numBlanks) {
+    indices.add(randomNumber(0, patternLength));
+  }
+  return Array.from(indices); 
 }
